b1b98798c9f1f5cb9c29fddb1ab4c94a034514eb
Hiddify1 <hiddify@gmail.com>
2023-03-23 08:23:23 +0100

    new: add startup window

---
+++ v2rayN/v2rayN/ViewModels/HomeWindowViewModel.cs
@@ -0,0 +1,101 @@
+﻿using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using MaterialDesignThemes.Wpf;
+using MaterialDesignThemes.Wpf.Transitions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using v2rayN.Views;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using v2rayN.ViewModels;
+
+
+namespace v2rayN.ViewModels
+{
+    public class HomeWindowViewModel : ViewModelBase
+    {
+        
+        public HomeWindowViewModel(ISnackbarMessageQueue snackbarMessageQueue)
+        {
+
+            DemoItems = new ObservableCollection<DemoItem>
+            {
+              new DemoItem(
+                    "Home",
+                    typeof(Home),
+                    selectedIcon: PackIconKind.Home,
+                    unselectedIcon: PackIconKind.HomeOutline)
+            };
+
+            
+            MainDemoItems = new ObservableCollection<DemoItem>
+            {
+            };
+
+            _demoItemsView = CollectionViewSource.GetDefaultView(DemoItems);
+            
+
+            
+            DismissAllNotificationsCommand = new AnotherCommandImplementation(
+                _ => DemoItems[0].DismissAllNotifications(),
+                _ => DemoItems[0].Notifications != null);
+
+            AddNewNotificationCommand = new AnotherCommandImplementation(
+                _ => DemoItems[0].AddNewNotification());
+
+            AddNewNotificationCommand.Execute(new object());
+        }
+        private readonly ICollectionView _demoItemsView;
+        private DemoItem? _selectedItem;
+        private int _selectedIndex;
+        private string? _searchKeyword;
+        private bool _controlsEnabled = true;
+
+
+        public ObservableCollection<DemoItem> DemoItems { get; }
+        public ObservableCollection<DemoItem> MainDemoItems { get; }
+
+        public DemoItem? SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
+        }
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set => SetProperty(ref _selectedIndex, value);
+        }
+        private bool _connectProgress = false;
+        public bool ConnectProgress {
+            get =>_connectProgress; 
+            set => SetProperty(ref _connectProgress, value); 
+        }
+        public bool ControlsEnabled
+        {
+            get => _controlsEnabled;
+            set => SetProperty(ref _controlsEnabled, value);
+        }
+
+        
+        public AnotherCommandImplementation DismissAllNotificationsCommand { get; }
+        public AnotherCommandImplementation AddNewNotificationCommand { get; }
+
+        private static IEnumerable<DemoItem> GenerateDemoItems(ISnackbarMessageQueue snackbarMessageQueue)
+        {
+            if (snackbarMessageQueue is null)
+                throw new ArgumentNullException(nameof(snackbarMessageQueue));
+
+            yield return null;
+        }
+    }
+}
---
+++ v2rayN/v2rayN/Views/Home.xaml.cs
@@ -0,0 +1,99 @@
+﻿using MaterialDesignColors.Recommended;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using v2rayN.ViewModels;
+
+namespace v2rayN.Views
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public static Snackbar Snackbar = new();
+        
+        public Home()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(() => Thread.Sleep(2500)).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue?.Enqueue("Welcome to Hiddify");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+            DataContext = new HomeWindowViewModel(MainSnackbar.MessageQueue!);
+
+        }
+        private void OnCopy(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Parameter is string stringValue)
+            {
+                try
+                {
+                    Clipboard.SetDataObject(stringValue);
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private void MenuToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuDarkModeButton_Click(object sender, RoutedEventArgs e)
+                    => ModifyTheme(DarkModeToggleButton.IsChecked == true);
+
+        private static void ModifyTheme(bool isDarkTheme)
+        {
+            var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+
+            theme.SetBaseTheme(isDarkTheme ? Theme.Dark : Theme.Light);
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void DarkModeToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DarkModeToggleButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CloseNotificationPanel_Click(object sender, RoutedEventArgs e) => NotificationPanel.Visibility = Visibility.Collapsed;
+
+        private void FAB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConnectVPN_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectVPN.Background= new SolidColorBrush(Color.FromRgb(0xFF,0xF2,0x67));
+            ((HomeWindowViewModel)DataContext).ConnectProgress = true;
+            Task.Factory.StartNew(() => Thread.Sleep(2500)).ContinueWith(t =>
+            {
+                ((HomeWindowViewModel)DataContext).ConnectProgress = false;
+                ConnectVPN.Background = new SolidColorBrush(Colors.LightGreen);
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
